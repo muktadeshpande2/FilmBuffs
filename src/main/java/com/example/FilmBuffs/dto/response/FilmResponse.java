493d37704b50f2d3884babb3d7fbdcd09676a0ca
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-20 18:41:21 +0530

    DTO classes

---
+++ src/main/java/com/example/FilmBuffs/dto/response/FilmResponse.java
@@ -0,0 +1,14 @@
+package com.example.FilmBuffs.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FilmResponse {
+
+    private String title;
+
+}
---
+++ src/main/java/com/example/FilmBuffs/dto/response/ReviewResponse.java
@@ -0,0 +1,14 @@
+package com.example.FilmBuffs.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReviewResponse {
+
+    private String review;
+
+}
