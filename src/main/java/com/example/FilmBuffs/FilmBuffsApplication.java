13a5208b627869886102ae32f8b6cea8c569fc8d
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-20 17:58:24 +0530

    Third Commit

---
+++ src/main/java/com/example/FilmBuffs/FilmBuffsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FilmBuffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmBuffsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmBuffsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FilmBuffs/FilmBuffsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FilmBuffs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmBuffsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
