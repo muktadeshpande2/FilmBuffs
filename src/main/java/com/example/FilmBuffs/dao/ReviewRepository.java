b8c900b76ac3002ff7ff4e458dad25197716b525
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-22 21:15:41 +0530

    Changed Id Datatype from Long to Integer and Added @Repository Annotation

--- src/main/java/com/example/FilmBuffs/dao/ReviewRepository.java
+++ src/main/java/com/example/FilmBuffs/dao/ReviewRepository.java
@@ -3,9 +3,11 @@ package com.example.FilmBuffs.dao;
 import com.example.FilmBuffs.model.Review;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ReviewRepository extends CrudRepository<Review, Long> {
+@Repository
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
 
     @Query(value = "select avg(rating) from review where film_id =?1", nativeQuery = true)
-    Double getReviewAverage(Long id);
+    Double getReviewAverage(Integer id);
 }
