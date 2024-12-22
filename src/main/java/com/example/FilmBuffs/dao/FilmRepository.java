5611668fc6f397c4ce23814e8aca146adc8b84e9
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-22 21:11:27 +0530

    Changed Id Datatype from Long to Integer

--- src/main/java/com/example/FilmBuffs/dao/FilmRepository.java
+++ src/main/java/com/example/FilmBuffs/dao/FilmRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FilmRepository extends CrudRepository<Film, Long> {
+public interface FilmRepository extends CrudRepository<Film, Integer> {
 
     Film findByTitle(String title);
 
