6e36c4741c8b45a9cfc28f40503fa3381dd05da0
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-22 20:35:52 +0530

    Added CriticResponse DTO class

---
+++ src/main/java/com/example/FilmBuffs/dto/response/CriticResponse.java
@@ -0,0 +1,15 @@
+package com.example.FilmBuffs.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CriticResponse {
+
+    private String name;
+
+    private String email;
+}
