7abf24077ab31be9ad801286010256f2d7f9cc6b
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-24 18:09:21 +0530

    Added Add, Update and Delete Film APIs along with @Service annotation

--- src/main/java/com/example/FilmBuffs/service/FilmService.java
+++ src/main/java/com/example/FilmBuffs/service/FilmService.java
@@ -3,11 +3,21 @@ package com.example.FilmBuffs.service;
 import com.example.FilmBuffs.dao.FilmRepository;
 import com.example.FilmBuffs.model.Film;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FilmService {
 
     @Autowired
     FilmRepository filmRepository;
+
+    public void addOrUpdateFilm(Film film) {
+        filmRepository.save(film);
+    }
+
+    public void deleteFilm(Film film) {
+        filmRepository.deleteById(film.getId());
+    }
 
     public Film findFilmByTitle(String title) {
         return filmRepository.findByTitle(title);
