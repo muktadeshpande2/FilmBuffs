7d4f7d587b3cff0f125358bbb0f66a0295cfb56d
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-24 18:43:03 +0530

    Added add and delete film APIs

--- src/main/java/com/example/FilmBuffs/controller/FilmController.java
+++ src/main/java/com/example/FilmBuffs/controller/FilmController.java
@@ -1,14 +1,14 @@
 package com.example.FilmBuffs.controller;
 
+import com.example.FilmBuffs.dto.request.AddFilmRequest;
+import com.example.FilmBuffs.dto.request.DeleteFilmRequest;
 import com.example.FilmBuffs.dto.response.FilmResponse;
 import com.example.FilmBuffs.service.FilmService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("film")
@@ -17,7 +17,26 @@ public class FilmController {
     @Autowired
     FilmService filmService;
 
+    //add, update, delete api
     //api to search film by title and genre
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addOrUpdateFilm(@RequestBody @Valid AddFilmRequest addFilmRequest) {
+        filmService.addOrUpdateFilm(addFilmRequest.toFilm());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("Film Added Successfully");
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteFilm(@RequestBody @Valid DeleteFilmRequest deleteFilmRequest) {
+        filmService.deleteFilm(deleteFilmRequest.toFilm());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Film Deleted Successfully");
+    }
 
     @GetMapping("/search/title")
     public ResponseEntity<FilmResponse> getFilmByTitle(@RequestParam String title) {
